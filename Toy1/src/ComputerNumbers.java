8f48eb726f91cfc96e922be6b3ba49a06f0b63d1
rlawltjd8547 <rlawltjd8547@nate.com>
2024-08-01 16:16:43 +0900

    #5.[컴퓨터의 랜덤난수를보관하기위한ComputerNumbers 클래스 생성]/[랜덤난수 기능을 스트림으로 재정의]

---
+++ Toy1/src/ComputerNumbers.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class ComputerNumbers {
+
+    private List<Integer> computerNumbers;
+
+    public ComputerNumbers() {
+    }
+
+    public ComputerNumbers(List<Integer> computerNumbers) {
+        this.computerNumbers = computerNumbers;
+    }
+
+    public List<Integer> getComputerNumbers() {
+        return computerNumbers;
+    }
+
+    public void setComputerNumbers(List<Integer> computerNumbers) {
+        this.computerNumbers = computerNumbers;
+    }
+
+}
