3eb0d1733767754a3f18299b201361a3ed3fdc83
rlawltjd8547 <rlawltjd8547@nate.com>
2024-09-02 06:52:56 +0900

    #10_1 [클래스 수정]

--- Toy2/src/OperatorPrecedence.java
+++ Toy2/src/OperatorPrecedence.java
@@ -9,18 +9,10 @@ public class OperatorPrecedence {
 
     // 연산자의 우선순위를 반환하는 메서드
     public static int getPrecedence(String operator) {
-        // 연산자가 리스트에 있으면 해당 연산자의 우선순위를 반환
-        switch (operator) {
-            case "*":
-            case "/":
-            case "%":
-                return 0; // 높은 우선순위
-            case "+":
-            case "-":
-                return 1; // 낮은 우선순위
-            default:
-                throw new IllegalArgumentException("Invalid operator: " + operator);
-        }
+        // 연산자가 리스트에 있으면 해당 연산자의 인덱스를 반환
+        int index = operators.indexOf(operator);
+        //3을 나누는이유는 *,/,% 는 나눠서 0이되기떄문에 0순위 나머지 1순위 / 연산자가 리스트에없으면 MAX_VALUE반환
+        return index != -1 ? index / 3 : Integer.MAX_VALUE; // 연산자가 없으면 가장 낮은 우선순위로 처리
     }
 
     // 두 연산자의 우선순위를 비교하는 메서드
@@ -28,8 +20,4 @@ public class OperatorPrecedence {
         return getPrecedence(op1) < getPrecedence(op2);
     }
 
-    // 주어진 연산자의 우선순위를 반환하는 메서드
-    public static int rankingOperator(String operator) {
-        return getPrecedence(operator);
-    }
 }
