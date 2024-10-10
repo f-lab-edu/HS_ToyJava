8a89b4901088eebf37d3fc91ade48a6322452d5f
rlawltjd8547 <rlawltjd8547@nate.com>
2024-10-10 18:36:27 +0900

    #25 [정렬 프로젝트 추가(gitIgnore처리 X)]

---
+++ SortProject/src/main/java/com/example/sortproject/SortProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.sortproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SortProjectApplication.class, args);
+    }
+
+}
---
+++ SortProject/src/test/java/com/example/sortproject/SortProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sortproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
